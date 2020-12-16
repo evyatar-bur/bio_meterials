a9bb3c743773e14200169d333adcd75f358d529d
evyatar-bur <65293193+evyatar-bur@users.noreply.github.com>
2020-12-16 17:06:35 +0200

    first

---
+++ load_and_smooth.m
@@ -0,0 +1,15 @@
+function [raw_data ,clean_data] = load_and_smooth(file,sheet_letter)
+% This function reads an excel file, from the requested sheet. 
+% The outputs of the function are the raw and the clean data from the excel file. 
+
+f = xlsread(file,sheet_letter);
+
+raw_data = {f(:,1:2),f(:,4:5),f(:,7:8)};
+clean_data = raw_data;
+
+for i = 1:3
+    clean_data{i}(:,2) = smoothdata(raw_data{i}(:,2));
+end
+
+close(f)
+end
